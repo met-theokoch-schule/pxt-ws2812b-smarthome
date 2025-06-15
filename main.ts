a180468e5c1ecca164a6fca36e7165c15133ad1d
met-theokoch-schule <91288462+met-theokoch-schule@users.noreply.github.com>
2025-06-15 19:54:04 +0200

    Aktualisiere main.ts, sendBuffer.asm, setBufferMode.asm

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-	
-})
+namespace ws2812b {
+    //% shim=sendBufferAsm
+    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
+    }
+
+    //% shim=setBufferMode
+    export function setBufferMode(pin: DigitalPin, mode: number) {
+
+    }
+}
